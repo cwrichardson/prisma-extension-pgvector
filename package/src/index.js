dfa0431abdd1f57c2a7fd0a4e39419196284bc08
Christopher Richardson <cwr@cwrichardson.com>
2024-05-22 14:06:37 -0400

    Pass config args down to the models. Include createVector method.

--- package/src/index.js
+++ package/src/index.js
@@ -1,6 +1,21 @@
 import { Prisma } from '@prisma/client';
 
-import * as store from './store/index.js';
+import * as store from './model-extensions/store/index';
+
+/**
+ * HOF to attach config args to methods
+ * 
+ * @param {import('$types/index.js').configArgs} configArgs
+ * @returns {Function}
+ */
+const withConfigArgs = (configArgs) => {
+    return (/** @type Function */ func) => {
+        return (/** @type any[] */ ...args) => {
+            const newArgs = [...args, configArgs];
+            func(...newArgs);
+        }
+    }
+}
 
 /**
  * Initialize PGVector as Prisma Extension
@@ -8,14 +23,44 @@ import * as store from './store/index.js';
  * @type {import('$types/index').withPGVector}
  */
 export const withPGVector = (args) => Prisma.defineExtension(function (client) {
+    /**
+     * add config args to methods
+     * 
+     * @type {import('$types/index').addProps}
+     */
+    const addProps = (methods, additionalProps) =>
+      Object.entries(methods).reduce((acc, [name, method]) => {
+        return {
+            ...acc,
+            [name]: withConfigArgs(additionalProps)(method)
+        }
+    }, {});
+
     const extensionMethods = {
-        ...store,
-        // @ts-expect-error
-        __$transaction: async (/** @type {any} */ ...args) => client.$transaction(...args)
-    }
+        ...store
+    };
+    const extensionMethodsWithProps = addProps(extensionMethods, args);
+    // @ts-expect-error
+    extensionMethodsWithProps['__$transaction'] = async (/** @type {any} */ ...args) => client.$transaction(...args)
+
+    // const queryMethods = {
+    //     ...store
+    // };
+    // 
+    // const extensionQueryMethods = Object.entries(queryMethods)
+    // .reduce((accumulator, [name, fn]) => {
+    //     return {
+    //         ...accumulator,
+    //         [name]: ({ ...prismaArgs }) => /** @type {import('$types/query').queryArgs} */ queryMethods[name]({
+    //             ...prismaArgs,
+    //             vectorFieldName: args.vectorFieldName
+    //         })
+    //     }
+    // }, {});
 
     return client.$extends({
         name: 'prisma-extension-pgvector',
-        model: { [args.modelName]: extensionMethods }
+        model: { [args.modelName]: extensionMethodsWithProps },
+        // query: { [args.modelName]: extensionQueryMethods }
     })
 })
