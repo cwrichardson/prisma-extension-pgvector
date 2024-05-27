417270946c074b162b941f46beb8f010bbc2ce85
Christopher Richardson <cwr@cwrichardson.com>
2024-05-27 14:53:14 -0400

    Wrap in try/catch.

--- dev-test/index.js
+++ dev-test/index.js
@@ -1,15 +1,22 @@
 import { PrismaClient } from '@prisma/client';
 import { withPGVector } from 'prisma-extension-pgvector';
 
+
 const prisma = new PrismaClient().$extends(withPGVector({
     modelName: 'vector',
     vectorFieldName: 'embedding'
 }));
 
-const insertedVector = await prisma.vector.createVector({
-    data: {
-        embedding: [1, 2, 3]
-    }
-});
-
-console.log(insertedVector);
+try {
+    const insertedVector = await prisma.vector.createVector({
+        data: {
+            embedding: [1, 2, 3]
+        }
+    });
+    
+    console.log(insertedVector);
+} catch (/** @type any */ e) {
+    console.log('Error inserting single vector');
+    console.log(e.message);
+    console.log(e.meta)
+}
