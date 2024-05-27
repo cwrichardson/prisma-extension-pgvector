4740c5101fe18bf193671aa1c4e6d1da643fe505
Christopher Richardson <cwr@cwrichardson.com>
2024-05-27 14:56:16 -0400

    Clean up comment-notes

--- package/types/model-extensions/store.d.ts
+++ package/types/model-extensions/store.d.ts
@@ -27,29 +27,7 @@ type reducedDataArgs<T, A extends Prisma.Args<T, 'create'>['data'],
   I extends keyof A,
   VF extends keyof A> = Pick<A, idField[I] | vectorFieldExtension[VF]>;
 
-// createVector
 export type createVectorArgs<T, A extends storeArgs> = {
   data: reducedDataArgs<A, configArgs['idFieldName'], configArgs['vectorFieldName']>
 } & { configArgs: configArgs };
-export type createVectorResult<T, A> = Prisma.Result<T, A, '$queryRawUnsafe'>;
-
-  // model.create['data'] minus everything except the id field, which must match the idfieldname below, if it's specified
-    // plus data[vectorFieldName] (which doesn't exist in the prisma type)
-  // omit all other model.create args
-  // add configArgs which includes vectorFieldName an an optional id field name, and arbitrary other parameters, all from type PGVectorInitArgs
-/* export type createVectorArgs<T, A> = {
-  data: {
-    [vectorFieldName]: string;
-    [idFieldName]: string;
-  };
-  configArgs: configArgs
-}
-export type createVectorResult<T, A> = Prisma.Result<T, A, 'create'>; */
-
-/* export type createVectorArgs<T, A> = {
-    data?: V extends keyof Prisma.Args<T, 'create'>['data']
-      ? Omit<Prisma.Args<T, 'create'>['data'], V> | { [V]: PrismaModelType }
-      : Prisma.Args<T, 'create'>['data'];
-} & Omit<Prisma.Args<T, 'create'>, 'data'>;
-
-export type createResult<T, A> = Prisma.Result<T, A, 'create'>; */
+export type createVectorResult<T, A> = Prisma.Result<T, A, '$queryRaw'>;
