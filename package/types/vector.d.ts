213bc6d859c15ed9743c271b57104ee98149f32e
Christopher Richardson <cwr@cwrichardson.com>
2024-06-04 09:44:49 -0400

    Fix variable field name types.

--- package/types/vector.d.ts
+++ package/types/vector.d.ts
@@ -1,3 +1,5 @@
+import { PrismaModelType } from '$types/prisma';
+
 /**
  * TODO: Would be better to import these from pgvector, but they don't seem
  *       to export them in the node module.
@@ -9,13 +11,13 @@ export type vector = vectorComponent[];
 
 /** for use in queryRaw */
 export type vectorEntry = {
-    [`${idFieldName}`]?: string,
-    [`${vectorFieldName}`]: vector
+    [K in `${idFieldName}`]: K extends PrismaModelType ? string | undefined : never;
+    [P in `${vectorFieldName}`]: P extends PrismaModelType ? vector : never;
 }
 
 /**
  * For extending model and query extensions to support the named vector field
  */
 export type vectorFieldExtension = {
-    [`${vectorFieldName}`]: string;
+    [K in `${vectorFieldName}`]: K extends PrismaModelType ? string : never;
 }
