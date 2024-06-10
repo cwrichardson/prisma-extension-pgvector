149923cf747cb0b33223334c989452fe4f3603c5
Christopher Richardson <cwr@cwrichardson.com>
2024-06-10 07:10:39 -0400

    Clean up `extended` type defs. Add createManyAndReturn types.

--- package/types/model-extensions/overrides.d.ts
+++ package/types/model-extensions/overrides.d.ts
@@ -4,29 +4,44 @@ import { configArgs } from '$types/index';
 import { vectorFieldExtension } from './vector';
 import { PrismaClient } from '@prisma/client/scripts/default-index';
 
-type extendedCreateDataArgs<
-  TModel,
-  A extends Prisma.Exact<T, 'create'>['data'],
-  VFName> = {
-  [K in keyof A]: A[K];
-} & { [K in keyof vectorFieldExtension[VFName]]: vectorFieldExtension[K] };
+/**
+ * Extensions for parts of larger native arguments
+ */
 
-type extendedSelectArgs<
-  T,
-  A extends Prisma.Exact<T, 'create'>['select'],
-  VFName> = {
-    [K in keyof A]: A[K];
+// create({ data: {} }) extended with vectorField
+type extendedCreateDataArgs<TModel, Args, VFName> = {
+  [K in keyof Args]: Args[K] extends Prisma.Exact<TModel, 'create'>['data']
+    ? Args[K]
+    : never;
+  } & { [K in keyof vectorFieldExtension[VFName]]: vectorFieldExtension[K] 
+};
+
+// <method>({ select: {} }) extended with vectorField
+type extendedSelectArgs<TModel, Args, VFName> = {
+    [K in keyof Args]: Args[K] extends Prisma.Exact<TModel, 'create'>['select']
+      ? Args[K]
+      : never;
 } & { [K in keyof vectorFieldExtension[VFName]]: { [K]: boolean }};
 
-type createExtendedArgs<
-  T,
-  A extends Prisma.Exact<A, Prisma.Args<T, 'create'>>,
-  VF
-> = {
-    data: extendedCreateDataArgs<T, A['data'], VF>,
-    select?: extendedSelectArgs<T, A['select'], VF>,
-} & Omit<Prisma.Exact<A, Prisma.Args<T, 'create'>>, 'data' | 'select'>;
+/**
+ * Extended args and return objects for override functions
+ */
 
-export type createArgs<T, A extends createExtendedArgs> = 
-  createExtendedArgs<T, A, configArgs['vectorFieldName']>
+// create
+export type createArgs<TModel, Args> = {
+    data: extendedCreateDataArgs<T, Args['data'],
+      configArgs['vectorFieldName']>,
+    select?: extendedSelectArgs<T, Args['select'],
+      configArgs['vectorFieldName']>,
+} & Omit<Prisma.Exact<A, Prisma.Args<T, 'create'>>, 'data' | 'select'>;
 export type createResult<T, A> = Prisma.PrismaPromise;
+
+// createManyAndReturn
+export type createManyAndReturnArgs<TModel, Args> = {
+    data: Array<extendedCreateDataArgs<TModel, Args['data'],
+      configArgs['vectorFieldName']>>,
+    select?: extendedSelectArgs<TModel, Args['select'],
+      configArgs['vectorFieldName']>,
+} & Omit<Prisma.Exact<A, Prisma.Args<T, 'createManyAndReturn'>>,
+  'data' | 'select'>;
+export type createManyAndReturnResult<T, A> = Prisma.PrismaPromise;
