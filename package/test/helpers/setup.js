cfdb89958a38d7092f39c6edaaef05cb8924738d
Christopher Richardson <cwr@cwrichardson.com>
2024-06-17 10:58:59 -0400

    Add  to set search_path to include extension

--- package/test/helpers/setup.js
+++ package/test/helpers/setup.js
@@ -1,6 +1,18 @@
 import { beforeEach } from 'vitest';
 
 import resetDb from './reset-db';
+import { beforeAll } from 'vitest';
+import prisma from './prisma';
+
+beforeAll(async () => {
+    await prisma.$executeRawUnsafe(`
+        DO $$
+            BEGIN
+                EXECUTE 'SET search_path TO '||current_setting('search_path')||',extension';
+            END
+        $$;
+    `)
+})
 
 beforeEach(async () => {
     await resetDb();
