845a1caa04b69320334b2c9941cfdc99aa823bbe
Christopher Richardson <cwr@cwrichardson.com>
2024-06-17 10:58:12 -0400

    Fix sql syntax error

--- package/test/helpers/reset-db.js
+++ package/test/helpers/reset-db.js
@@ -3,6 +3,6 @@ import prisma from './prisma';
 export default async () => {
     await prisma.$transaction([
         prisma.vector.deleteMany(),
-        prisma.$executeRawUnsafe(`ALTER "Vector_id_seq" RESTART WITH 1`)
+        prisma.$executeRawUnsafe(`ALTER SEQUENCE "Vector_id_seq" RESTART WITH 1`)
     ])
 }
