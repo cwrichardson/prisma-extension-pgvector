028f74abef8662bdbd7c856c00ee4fedad492caf
Christopher Richardson <cwr@cwrichardson.com>
2024-06-17 14:54:46 -0400

    Add updateManyVectors tests

--- package/test/store.test.js
+++ package/test/store.test.js
@@ -50,7 +50,7 @@ describe('store', async () => {
                 ]
             });
 
-            expect(countObj).toStrictEqual({ count: 2 })
+            expect(countObj).toStrictEqual({ count: 2 });
         }),
         it('should support id specified in only middle of 3 or more', async () => {
             const countObj = await prisma.vector.createManyVectors({
@@ -61,7 +61,47 @@ describe('store', async () => {
                 ]
             });
 
-            expect(countObj).toStrictEqual({ count: 3 })
+            expect(countObj).toStrictEqual({ count: 3 });
+        })
+    }),
+    describe('createManyVectorsAndReturn', () => {
+        it('should create vectors and return array of vector objects', async () => {
+            const vectors = await prisma.vector.createManyVectorsAndReturn({
+                data: [
+                    { embedding: [1,2,3] },
+                    { embedding: [4,5,6] }
+                ]
+            });
+
+            expect(vectors).toHaveLength(2);
+            expect(vectors).toContainEqual(expect.objectContaining({ embedding: [1,2,3]}));
+            expect(vectors).toContainEqual(expect.objectContaining({ embedding: [4,5,6]}));
+        }),
+        it('should create vectors and ids and return vector objects', async () => {
+            const vectors = await prisma.vector.createManyVectorsAndReturn({
+                data: [
+                    { id: 10, embedding: [1,2,3] },
+                    { id: 11, embedding: [4,5,6] }
+                ]
+            });
+
+            expect(vectors).toHaveLength(2);
+            expect(vectors).toContainEqual({ id: 10, embedding: [1,2,3]});
+            expect(vectors).toContainEqual({ id: 11, embedding: [4,5,6]});
+        }),
+        it('should support id specified in only middle of 3 or more', async () => {
+            const vectors = await prisma.vector.createManyVectorsAndReturn({
+                data: [
+                    { embedding: [1,2,3] },
+                    { id: 20, embedding: [4,5,6] },
+                    { embedding: [7,8,9] }
+                ]
+            });
+
+            expect(vectors).toHaveLength(3);
+            expect(vectors).toContainEqual(expect.objectContaining({ embedding: [1,2,3]}));
+            expect(vectors).toContainEqual({ id: 20, embedding: [4,5,6]});
+            expect(vectors).toContainEqual(expect.objectContaining({ embedding: [7,8,9]}));
         })
     }),
     describe('updateVector', () => {
@@ -69,24 +109,59 @@ describe('store', async () => {
             const newVector = await prisma.vector.updateVector({
                 data: { embedding: [7,8,9] },
                 where: { id: 1 }
-            })
+            });
 
             expect(newVector).toStrictEqual({
                 id: 1,
                 embedding: [7, 8, 9]
-            })
+            });
         }),
         it('requires id in where', async () => {
             // @ts-expect-error
             await expect(prisma.vector.updateVector({
                 data: { embedding: [1,2,3]}
-            })).rejects.toThrowError('where: { <idFieldName>: <id>  } is required')
+            })).rejects.toThrowError('where: { <idFieldName>: <id>  } is required');
         }),
         it('requires data', async () => {
             // @ts-expect-error
             await expect(prisma.vector.updateVector({
                 where: { id: 1 }
-            })).rejects.toThrowError('data object is required.')
+            })).rejects.toThrowError('data object is required.');
+        })
+    }),
+    describe('updateManyVectors', () => {
+        it('should update many vectors and return count', async () => {
+            const count = await prisma.vector.createManyVectors({
+                data: [
+                    { id: 25, embedding: [1,2,3] },
+                    { id: 45, embedding: [1,2,3] }
+                ]
+            })
+            .then(async (c) => {
+                return await prisma.vector.updateManyVectors({
+                    data: [
+                        { id: 25, embedding: [4,5,6] },
+                        { id: 45, embedding: [7,8,9] }
+                    ]
+                })
+            });
+
+            expect(count).toStrictEqual({ count: 2 });
+        }),
+        it('should throw if any object lacks an id', async () => {
+            const count = await prisma.vector.createManyVectors({
+                data: [
+                    { id: 25, embedding: [1,2,3] },
+                    { id: 45, embedding: [1,2,3] }
+                ]
+            });
+
+            await expect(prisma.vector.updateManyVectors({
+                data: [
+                    { id: 25, embedding: [4,5,6 ]},
+                    { embedding: [7,8,9] }
+                ]
+            })).rejects.toThrowError('UpdateManyVectors requires every object to have an id');
         })
     })
 })
