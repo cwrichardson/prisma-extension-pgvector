8cd6bd979a1fa97ab2fec6b187671b120eb641ae
Christopher Richardson <cwr@cwrichardson.com>
2024-06-17 20:29:57 -0400

    Add findNearestNeighbors tests

--- package/test/query.test.js
+++ package/test/query.test.js
@@ -1,6 +1,7 @@
 import { describe, expect, it } from 'vitest';
 import prisma from './helpers/prisma';
 import { expectTypeOf } from 'vitest';
+import { beforeEach } from 'vitest';
 
 describe('query', async () => {
     describe('getVectorsById', () => {
@@ -69,6 +70,65 @@ describe('query', async () => {
                 { id: 25, embedding: [1,2,3] },
                 { id: 45, embedding: [1,2,3] }
             ]);
+        })
+    }),
+    describe('findNearestNeighbors', () => {
+        beforeEach(async () => {
+            await prisma.vector.createManyVectors({
+                data: [
+                    { id: 1, embedding: [1,2,3] },
+                    { id: 2, embedding: [4,5,6] },
+                    { id: 3, embedding: [7,8,9] },
+                    { id: 4, embedding: [1,2,3] },
+                    { id: 5, embedding: [4,5,6] },
+                    { id: 6, embedding: [7,8,9] },
+                    { id: 7, embedding: [1,2,3] },
+                    { id: 8, embedding: [4,5,6] },
+                    { id: 9, embedding: [7,8,9] }
+                ]
+            })
+        }),
+        it('should find 2 if we take 2', async () => {
+            await expect(prisma.vector.findNearestNeighbors({
+                from: [1,2,3],
+                take: 2
+            })).resolves.toStrictEqual([
+                { id: 1, embedding: [1,2,3] },
+                { id: 4, embedding: [1,2,3] }
+            ])
+        }),
+        it('should find nearest given specific ids', async () => {
+            await expect(prisma.vector.findNearestNeighbors({
+                from: [1,2,3],
+                where: { id: { in: [ 2, 3, 4, 7, 8 ] } }
+            })).resolves.toStrictEqual([
+                { id: 4, embedding: [1,2,3] },
+                { id: 7, embedding: [1,2,3] },
+                { id: 2, embedding: [4,5,6] },
+                { id: 8, embedding: [4,5,6] },
+                { id: 3, embedding: [7,8,9] }
+            ])
+        }),
+        it('should find by inner product', async () => {
+            await expect(prisma.vector.findNearestNeighbors({
+                from: [1,2,3],
+                orderBy: 'InnerProduct',
+                take: 2
+            })).resolves.toStrictEqual([
+                { id: 1, embedding: [1,2,3] },
+                { id: 4, embedding: [1,2,3] }
+            ])
+        })
+        it('should find by inner product, given specific ids', async () => {
+            await expect(prisma.vector.findNearestNeighbors({
+                from: [1,2,3],
+                orderBy: 'InnerProduct',
+                where: { id: { in: [ 6, 7, 8] } },
+                take: 2
+            })).resolves.toStrictEqual([
+                { id: 7, embedding: [1,2,3] },
+                { id: 8, embedding: [4,5,6] }
+            ])
         })
     })
 })
