830407524045da84d88b56ad643edbca2c1fd479
Christopher Richardson <cwr@cwrichardson.com>
2024-06-21 08:48:18 -0400

    Who lints the linter?

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,37 +7,37 @@ import tseslint from 'typescript-eslint';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const gitIgnorePath = path.resolve(__dirname, ".gitignore");
+const gitIgnorePath = path.resolve(__dirname, '.gitignore');
 
 export default [
-  includeIgnoreFile(gitIgnorePath),
-  pluginJs.configs.recommended,
-  {
-    plugins: {
-      '@stylistic/js': stylistcJs
-    },
-    languageOptions: {
-      parserOptions: {
-        sourceType: 'module',
-        ecmaVersion: 2022
-      }
-    },
-    rules: {
-      indent: ['error', 'tab'],
-      quotes: ['error', 'single'],
-      semi: ['warn', 'always', { omitLastInOneLineBlock: true }],
-      'no-case-declarations': 0,
-      'no-undef': 'error',
-      'no-unused-vars': [
-        'warn',
-        {
-          'vars': 'all',
-          'args': 'after-used',
-          'ignoreRestSiblings': true,
-          'argsIgnorePattern': '^_'
-        }
-      ]
-    }
-  },
-  ...tseslint.configs.recommended,
+	includeIgnoreFile(gitIgnorePath),
+	pluginJs.configs.recommended,
+	{
+		plugins: {
+			'@stylistic/js': stylistcJs
+		},
+		languageOptions: {
+			parserOptions: {
+				sourceType: 'module',
+				ecmaVersion: 2022
+			}
+		},
+		rules: {
+			indent: ['error', 'tab'],
+			quotes: ['error', 'single'],
+			semi: ['warn', 'always', { omitLastInOneLineBlock: true }],
+			'no-case-declarations': 0,
+			'no-undef': 'error',
+			'no-unused-vars': [
+				'warn',
+				{
+					'vars': 'all',
+					'args': 'after-used',
+					'ignoreRestSiblings': true,
+					'argsIgnorePattern': '^_'
+				}
+			]
+		}
+	},
+	...tseslint.configs.recommended,
 ];
