9b57d1a18b0d84ee6c4c5a26253a692f77bd9c88
Christopher Richardson <cwr@cwrichardson.com>
2024-06-26 07:40:01 -0400

    Move prisma generate to CI script. Do prisma migrate deploy.

--- scripts/run-unit-tests.sh
+++ scripts/run-unit-tests.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 BASEDIR="$(cd "$(dirname "$0")/.." && pwd)"
 SCRIPTSDIR="$BASEDIR/scripts"
+SCHEMA="$BASEDIR/package/test/prisma/schema.prisma"
 source $SCRIPTSDIR/setenv.sh
 cd $BASEDIR/package/test && docker-compose up -d
 echo '🟡 - Waiting for database to be ready...'
 $SCRIPTSDIR/wait-for-it.sh "${DATABASE_URL}" -- echo '🟢 - Database is ready!'
-cd $BASEDIR/package && pnpm test
+cd $BASEDIR/package
+pnpx prisma generate --schema $SCHEMA
+pnpx prisma migrate deploy --schema $SCHEMA
+pnpm test
